1db47dd9133a526abe6931e3a19959f9dfdd2151
damianp63 <damianpodrazka@wp.pl>
2016-08-11 16:04:19 +0200

    zadanie 3

---
+++ person-list-stats.rb
@@ -0,0 +1,27 @@
+require 'csv'
+persons_list = []
+CSV.foreach("baza_danych_3.csv") do |person|
+  persons_list<<Person.new(first_name: person[0],last_name: person[1])
+end
+
+first_name_list=[]
+last_name_list=[]
+persons_list.each do |person|
+  first_name_list << person.first_name
+  last_name_list << person.last_name
+end
+
+uniq_first_name_list=persons_list.uniq{|person| person.first_name}
+uniq_last_name_list=persons_list.uniq{|person| person.last_name}
+
+puts "Nazwiska:"
+uniq_last_name_list.each do |person|
+  duplication=last_name_list.count(person.last_name)
+  puts "#{person.last_name} (#{duplication})"
+end
+
+puts "Imiona:"
+uniq_first_name_list.each do |person|
+  duplication=first_name_list.count(person.first_name)
+  puts "#{person.first_name} (#{duplication})"
+end
--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(first_name: last_name:)
+  def initialize(first_name:, last_name:)
     self.first_name = :first_name
     self.last_name = :last_name
   end
